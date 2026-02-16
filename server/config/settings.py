47d98a7d88b60119c9af8652928565be06fe2bca
erybie222 <erybie222@gmail.com>
2026-02-16 10:59:12 +0100

    feat: integrate drf-spectacular for API documentation and update URL routing

--- server/config/settings.py
+++ server/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_spectacular',
     'cities_light',
     'accounts',
     'products',
@@ -134,4 +135,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Ecommerce Shop API',
+    'DESCRIPTION': 'API documentation for Ecommerce Shop',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
